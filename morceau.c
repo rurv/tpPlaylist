8635972bf158f5f619a35586fcd8bccab32849d4
rurv <rubconstant@gmail.com>
2026-02-17 00:41:02 +0100

    commit

--- morceau.c
+++ morceau.c
@@ -10,7 +10,6 @@
 Morceau* creerMorceau (void) {
     Morceau* m = malloc(sizeof(Morceau));
     if (m == NULL) return NULL;
-
     char titre[MC_TITRE_MAX];
     saisieStr(titre, "\nSaisir titre du morceau", MC_TITRE_MAX);
     m->titre = malloc((strlen(titre) + 1) * sizeof(char));
@@ -20,7 +19,6 @@ Morceau* creerMorceau (void) {
         return NULL;
     }
     strcpy(m->titre, titre);
-
     char artiste[MC_ARTISTE_MAX];
     saisieStr(artiste, "\nSaisir artiste", MC_ARTISTE_MAX);
     m->artiste = malloc((strlen(artiste) + 1) * sizeof(char));
@@ -32,11 +30,9 @@ Morceau* creerMorceau (void) {
         return NULL;
     }
     strcpy(m->artiste, artiste);
-
     m->duree = saisieInt("\nSaisir duree du morceau en secondes", MC_DUREE_MIN, MC_DUREE_MAX);
     m->annee = saisieInt("\nSaisir annee de sortie", MC_ANNEE_MIN, MC_ANNEE_MAX);
     m->nbRef = 1;
-
     return m;
 }
 
@@ -50,4 +46,3 @@ void libererMorceau (Morceau* m) {
         m = NULL;
     }
 }
-
